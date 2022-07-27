0a5e7180bcd2bbc399451d6be94372e3e17f22b0
Mohit Pawar <83939027+mohitpawar17@users.noreply.github.com>
2022-07-27 23:00:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('lr_assignment1.pkl','rb')) 
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    exp = float(request.args.get('exp'))
+    
+    prediction = model.predict([[exp]])
+    
+        
+    return render_template('index.html', prediction_text='Regression Model  has predicted price for given square-feet is : {}'.format(prediction))
+
+
+app.run()
